02ddeb84fb9ab73c02a3ddb8f88c837bd3c5caa7
Thinker-as <thinker.ne@gmail.com>
2018-12-01 23:53:29 +0300

    remote bean with configurations for Thymeleaf in BlogMvcApplicatio

--- src/main/java/blog/BlogMvcApplication.java
+++ src/main/java/blog/BlogMvcApplication.java
@@ -2,29 +2,10 @@ package blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.ViewResolver;
-import org.thymeleaf.spring4.SpringTemplateEngine;
-import org.thymeleaf.spring4.view.ThymeleafViewResolver;
-import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 @SpringBootApplication
 public class BlogMvcApplication {
     public static void main(String[] args) {
         SpringApplication.run(BlogMvcApplication.class, args);
-    }
-
-    @Bean
-    public ViewResolver viewResolver() {
-        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
-        templateResolver.setPrefix("/public/css/");
-        templateResolver.setSuffix(".css");
-
-        SpringTemplateEngine engine = new SpringTemplateEngine();
-        engine.setTemplateResolver(templateResolver);
-
-        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
-        viewResolver.setTemplateEngine(engine);
-        return viewResolver;
     }
 }
